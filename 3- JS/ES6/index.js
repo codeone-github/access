3c7dc08c159f8d75659120d8a495fc0df839d035
salman18 <salmanaleemkhan786@gmail.com>
2020-01-24 00:49:20 +0530

    Array helper methods in ES6

--- 3- JS/ES6/index.js
+++ 3- JS/ES6/index.js
@@ -108,3 +108,75 @@ console.log(greeting);
 // Have a great Life ahead, be cool as always
 // `;
 // console.log(greeting);
+
+
+//Array helper methods in ES6
+ 
+// forEach
+// forEach accept a function iterator that loop through each item in an array
+
+const array = [1,2,3,4]
+array.forEach(item => console.log(item))
+
+// so above code will print 1 2 3 4. You can also create a function and pass the reference into forEach, like this
+
+const items = (item) => { console.log(item) }
+array.forEach(items)
+// It will produce the same result which is 1 2 3 4.
+
+// Map
+// Map will loop through each item of array, same like forEach but Map returns the value of the array.
+
+const arr = [{name:"James",age:18}, {name:"Alice",age:20}]
+const person_names = arr.map(person => person.name);
+
+console.log(person_names);
+
+// That will print
+// ["Salman”,”Taj”]
+// Easy? One of the biggest advantage to use Map over a old-school for loop is that you don’t hold temporary variable. Let’s do a reverse example for above code using for loop.
+
+const arrayOfObjects = [{name:"Salman",age:24}, {name:"Taj",age:18}]
+let temp = []
+for(let i = 0; i < arrayOfObjects.length; i++){
+    temp.push(arrayOfObjects[i].name)
+}
+console.log(temp)
+// See? Map is no magic. Map significantly reduced the amount of code and avoided the use of variable holder.
+
+
+// filter
+// Filter return array based on the boolean of the comparison
+
+const fruits = [
+ {name:"banana",color:"yellow"},
+ {name:"watermelon",color:"red"},
+ {name:"orange",color:"orange"}
+]
+const filteredFruits = fruits.filter(fruit => fruit.color === "yellow");
+console.log(filteredFruits)
+// Above code will return [{name:”banana”,color:”yellow”}]. Filter is often use in filtering and sorting of a list.
+
+
+// find
+// find will return the record if a particular element is found in the array.
+
+let x = [
+   {name:"jay",age:1},
+   {name:"Veeru",age:30}
+]
+x.find(obj => obj.name === "jay")
+// x will return {name"jay",age:1}
+
+// reduce
+// reduce is the most flexible helper, we can probably reimplement all the other helpers by just using reduce. Since reduce is the most complicated one, let’s do a classic normal for loop first.
+const numbers = [1,2,3,4,5]
+let sum = 0
+for(let i=0;i<numbers.length;i++){
+   sum += numbers[i]
+}
+console.log(sum)
+
+// The sum will print 15. Let’s do it in one line using reduce.
+// numbers.reduce((sum, numbers) => sum + numbers, 0)
+// reduce takes two arguments, the first argument is the iterator function, which in above case sum is the accumulator and numbers is an array, the second argument is the initial value, for our case we want it to be zero.
